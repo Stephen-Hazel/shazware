4ca814b79e0fbdd783d45913c17bb18bfa37cc06
Stephen Hazel <stephen.hazel@gmail.com>
2026-01-10 12:43:52 -0800

    fixin b.sh

--- b.sh
+++ b.sh
@@ -1,39 +1,47 @@
-#!/bin/env php
-<?php
-// b - build with flatpak-builder n stuff
-// args:   o   build only - for release to flathub
-//         c   wipe config dir - for full reinstall
+#!/bin/php
+<?php # b.sh - build with flatpak-builder n stuff
+      # args:  c  wipe config dir - for full reinstall
+      #        d  build with debugging so ya can gdb inside it
    $arg = '';   if ($argc > 1)  $arg = $argv [1];
 
-// repo name n app triplet
-   $repo = "sh_repo";   $app = "app.shaz.shazware";
-   $f = "flatpak";      $fb = "flatpak-builder";
+// app triplet n fpak cmds
+   $app = "app.shaz.shazware";
+   $f = "flatpak";   $fb = "$f-builder";
 
-// source => _build
-#  system ("rm -fr _build .$fb");
-   system ("rm -fr _build");
-   system ("mkdir  _build");
-   system ("$fb    _build _manif", $rc);         // _manif is build manifest
-#  system ("rm -fr .$fb");
-   if (($rc != 0) || ($arg == 'o'))  exit;
-                                       // build error :( or build only
-// uninstall old app
+   system ("rm -fr _build .$fb");      // cleanup
+
+echo "...uninstall old one\n";
    system ("$f uninstall -y $app");
 
-// _build => _repo  (flathub pr does this, too)
-// add _build n release manifest to flathub pr's git repo
-   system ("rm -fr _xb _repo");        // unused build dir _xb for fb
-   system ("mkdir  _xb _repo");
-   system ("$fb --repo=_repo _xb $app.json");    // flathub release manifest
-#  system ("rm -fr    _build _xb .$fb");
-   system ("rm -fr    _build _xb");
+   if ($arg == 'd') {
+/*    DEBUGGIN !  then go...
+      flatpak run --command=sh --devel --filesystem=$(pwd) app_triplet
+      gdb /app/bin/pianocheetah
+      set logging enabled on
+      thread apply all backtrace
+      run
+      where
+      bt full
+*/    system ("mkdir _build");
+      system ("$fb --user --force-clean --install _build fpak.dbg", $rc);
+      system ("$f install --reinstall --user --assumeyes ".
+              "/home/sh/src/pianocheetah/.$fb/cache $app");
+      system ("$f install --reinstall --user --assumeyes ".
+              "/home/sh/src/pianocheetah/.$fb/cache $app".".Debug");
+      exit;
+   }
 
-// add repo, install, remove n rm repo  (just tryna be neat)
-   system ("$f remote-add --no-gpg-verify $repo _repo");
-   system ("$f install --noninteractive   $repo $app");
-   system ("$f --force remote-delete      $repo");
-   system ("rm -fr _repo");
+// source => _build => install
+   system ("mkdir _build");
 
-// cleanup app's .var/app/ dir for full reset
+echo "...compilin n installin\n";
+   system ("$fb --user --install _build fpak", $rc);
+   if ($rc != 0)  exit;                // build error :(
+
+echo "...cleanup\n";
+   system ("rm -fr _build .$fb");
    if ($arg != 'c')  exit;
+
+// cleanup .var/app/ dir for full reset (wipe config files etc)
+echo "...reset config files\n";
    system ("rm -fr ~/.var/app/$app");
